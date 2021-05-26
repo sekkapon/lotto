c37802772b048269ec0a0ae0272ffc5eb12d813d
iambulldog <mommam3937@gmail.com>
2021-05-26 21:25:05 +0700

    แต่งหน้าเล่น

--- app/Views/template/tem_frontend/navbar.php
+++ app/Views/template/tem_frontend/navbar.php
@@ -1,45 +1,48 @@
 <div class="app-sidebar sidebar-shadow">
-                <div class="app-header__logo">
-                    <div class="logo-src"></div>
-                    <div class="header__pane ml-auto">
-                        <div>
-                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span class="hamburger-box">
-                                        <span class="hamburger-inner"></span>
-                                    </span>
-                                </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="app-header__mobile-menu">
-                    <div>
-                        <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-                                <span class="hamburger-box">
-                                    <span class="hamburger-inner"></span>
-                                </span>
-                            </button>
-                    </div>
-                </div>
-                <div class="app-header__menu">
-                    <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                    </button>
+    <div class="app-header__logo">
+        <div class="logo-src"></div>
+        <div class="header__pane ml-auto">
+            <div>
+                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
                     </span>
-                </div>
-                <div class="scrollbar-sidebar">
-                    <div class="app-sidebar__inner">
-  
-                    สวัสดีคะ คุณ AAAA
-4,460.00 
-
-งวดประจำวันที่ 01-06-2021
-เครดิต	5,000.00
-ยอดพนัน	0.00
-ยอดได้-เสีย	-540.00
-คงเหลือ	4,460.00
-                    </div>
-                </div>
+                </button>
             </div>
+        </div>
+    </div>
+    <div class="app-header__mobile-menu">
+        <div>
+            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </button>
+        </div>
+    </div>
+    <div class="app-header__menu">
+        <span>
+            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                <span class="btn-icon-wrapper">
+                    <i class="fa fa-ellipsis-v fa-w-6"></i>
+                </span>
+            </button>
+        </span>
+    </div>
+    <div class="scrollbar-sidebar">
+        <div class="app-sidebar__inner">
+            <div class="card-bodyM">
+                <h5 class="card-title"> สวัสดีค่ะ คุณ AAAA </h5>
+                <ul class="list-group">
+                    <li class="justify-content-between list-group-item"> งวดประจำวันที่ 01-06-2021 </li>
+                    <li class="justify-content-between list-group-item"> เครดิต <font size="+2"><span class="badge badge-info badge-pill">5,000.00</span></font></li>
+                    <li class="justify-content-between list-group-item"> ยอดพนัน <font size="+2"><span class="badge badge-secondary badge-pill">0.00</span></font></li>
+                    <li class="justify-content-between list-group-item"> ยอดได้-เสีย <font size="+2"><span class="badge badge-danger badge-pill">-540.00</span></font></li>
+                    <li class="justify-content-between list-group-item"> คงเหลือ <font size="+2"><span class="badge badge-success badge-pill">4,460.00</span></font></li>
+                </ul>
+            </div>
+
+
+        </div>
+    </div>
+</div>
