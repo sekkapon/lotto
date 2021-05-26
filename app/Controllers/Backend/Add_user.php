2e545e850444df896cd15529c009f397aef423aa
sekkapon <farnfarnsekkapon@hotmail.com>
2021-05-26 19:51:33 +0700

    add user 26/5

--- app/Controllers/Backend/Add_user.php
+++ app/Controllers/Backend/Add_user.php
@@ -13,10 +13,43 @@ class Add_user extends BaseController
     public function addUser()
     {
         $arrData = $this->request->getPost('arrData');
-
-        $password =   $this->My_Library->hash_password($arrData['password']);
         // echo '<pre>';
         // print_r($this->request->getPost());
+        // die;
+
+        $dataQuery = array(
+            'tableDB' => 'tb_user',
+            'whereData' => [
+                'username' => $arrData['username'],
+            ],
+            'data' => [
+                'username' => $arrData['username'],
+                'firstname' => $arrData['firstname'],
+                'phone' => $arrData['phone'],
+                'password' =>  $this->My_Library->hash_password($arrData['password']),
+                'role' => 'member',
+                'create_time' => time(),
+                'status' => 1
+            ]
+        );
+        // if ($this->My_Query->checkHaveData($dataQuery) == TRUE) {
+        //     echo json_encode(array('code' => 3, 'msg' => 'ยูสเซอร์เนมถูกใช้งานแล้ว'));
+        //     die;
+        // }
+        // if ($this->My_Query->insertData($dataQuery) == FALSE) {
+        //     echo json_encode(array('code' => 2, 'msg' => 'ไม่สามารถเพิ่มข้อมูลได้กรุณาติดต่อโปรแกรมเมอร์'));
+        //     die;
+        // }
+        foreach ($arrData['dataConfig'] as $key => $valueDataConfig) {
+            $cutData = explode('_', $valueDataConfig['name']);
+            $typeLotto = $cutData[0];
+            echo '<pre>';
+            print_r($typeLotto);
+        }
+
         die;
+        // echo '<pre>';
+        // print_r($this->My_Query->insertData($dataQuery));
+        // die;
     }
 }
